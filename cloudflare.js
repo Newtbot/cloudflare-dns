2e5c0de95ad209a6a87ebcc1b9abf89b9a0b2fc9
newtbot <limtiseng@gmail.com>
2025-02-20 22:54:37 +0800

    wip

--- cloudflare.js
+++ cloudflare.js
@@ -1,4 +1,5 @@
 "use strict";
+const config = require("@simpleworkjs/conf");
 const axios = require("axios");
 require("dotenv").config();
 
@@ -7,11 +8,28 @@ require("dotenv").config();
 // # if the IP and my current Public IP is different i should use CF API and update it
 
 class CloudFlare {
-	constructor(apiToken, zoneId, baseUrl) {
-		this.apiToken = apiToken;
-		this.zoneId = zoneId;
-		this.baseUrl = baseUrl;
+	// constructor(apiToken, zoneId, baseUrl) {
+	// 	this.apiToken = apiToken;
+	// 	this.zoneId = zoneId;
+	// 	this.baseUrl = baseUrl;
+	// }
+	constructor(config) {
+		this.config = config
+		this.apiToken = process.env.API_TOKEN
 	}
+	//write the logic to comapre both public ip if one is different or blah update accordingly
+	async compareIP() {
+		try {
+			if (this.getIP() !== this.getDNS()) {
+				//call updateDNS
+				const newIP = await this.getIP();
+				this.updateDNS(newIP);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	//To get DNS RECORD ID.
 	// async getAllDNS(){
 
@@ -24,9 +42,8 @@ class CloudFlare {
 	//                 'Authorization': "Bearer " + this.apiToken
 	//             }
 	//         };
-	//         // console.log(`${this.baseUrl}${this.zoneId}/dns_records`)
 	//         let cfRes = await axios.request(options)
-	//         console.log(cfRes.data.result.id)
+	//         console.log(cfRes.data.result)
 
 	//     } catch (error) {
 	//         console.log(error.response); // this is the main part. Use the response property from the error object
@@ -34,54 +51,40 @@ class CloudFlare {
 
 	// }
 
+	//Use as helper by compareIp()
 	async getIP() {
 		try {
 			const res = await axios.get("https://wtfismyip.com/json");
 			console.log(res.data.YourFuckingIPAddress);
-            return res.data.YourFuckingIPAddress;
+			return res.data.YourFuckingIPAddress;
 		} catch (error) {
 			console.log(error.response);
-				}
+		}
 	}
-	async getDNS() {
-		//to get individual domain details you would need dns_record_id which can only be obtained from list all domain??
-		// console.log(this.apiToken , this.zoneId)
+	async getDNS(domainName) {
 		try {
+			const subDomain = domainName.split('.')[0];
 			const options = {
 				method: "GET",
-				url: `${this.baseUrl}${this.zoneId}/dns_records/${process.env.DNS_RECORD_ID}`,
+				url: `${config.domains baseUrl}/${this.zoneId}/dns_records/${config.domains+ "." + subDomain.cloudflare_Dns_Id}`,
 				headers: {
 					"Content-Type": "application/json",
-					Authorization: "Bearer " + this.apiToken,
+					Authorization: "Bearer " + this.apiToken
 				},
 			};
 			let cfRes = await axios.request(options);
-			// console.log(cfRes.data.result.content); //PUBLIC IP
-            return cfRes.data.result.content; // will return ip currently being used by the subdomain
+			console.log(cfRes)
+			return cfRes.data.result.content; // will return ip currently being used by the subdomain
 		} catch (error) {
 			console.log(error.response);
 		}
 	}
-	//write the logic to comapre both public ip if one is different or blah update accordingly
-	async compareIP() {
-		try {
-			if (this.getIP() !== this.getDNS()){
-				//call updateDNS
-				const newIP = await this.getIP();
-				this.updateDNS(newIP)
-			}
 
-		} catch (error) {
-			console.log(error)
-
-		}
-	}
-
-    //the logic will be called in compareIP 
+	//the logic will be called in compareIP
 	async updateDNS(ip) {
 		try {
 			//required!
-			// content 
+			// content
 			// name
 			// type
 			// comment
@@ -93,12 +96,12 @@ class CloudFlare {
 					"Content-Type": "application/json",
 					Authorization: "Bearer " + this.apiToken,
 				},
-				data:{
+				data: {
 					content: ip,
 					name: "wireguard.teeseng.uk",
-					type:"A",
-					comment:"For WG VPN purposes"
-				}
+					type: "A",
+					comment: "For WG VPN purposes",
+				},
 			};
 			let cfRes = await axios.request(options);
 		} catch (error) {
@@ -111,16 +114,54 @@ module.exports = CloudFlare;
 
 if (require.main === module) {
 	(async function () {
+		//write a for loop that runs through my subdomains?
 		try {
-			let cloudflare = new CloudFlare(
-				process.env.API_TOKEN,
-				process.env.ZONE_ID,
-				process.env.BASE_URL
+			const domainValues = Object.values(config.domains).map(
+				(obj) => obj.domain
 			);
-			cloudflare.compareIP()
+			// console.log(config)
+
+			// const domainValues = Object.values(config.domains)
+			// console.log(domainValues[0])
+
+			//for loop
+			for (let domainName of domainValues) {
+				// console.log(domainName)
+				//write a logic to compare ip for domain in the loop
+				//need to pass 
+				let cloudflare = new CloudFlare(config);
+				cloudflare.getDNS(domainName)
+
+
+			}
+
+			let cloudflare = new CloudFlare();
+			// process.env.API_TOKEN,
+			// process.env.ZONE_ID,
+			// process.env.BASE_URL
+			// cloudflare.getAllDNS()
+			// cloudflare.compareIP()
 			// cloudflare.getIP()
 		} catch (error) {
 			console.log("IIFE Error:", error);
 		}
 	})();
 }
+
+// if (require.main === module) {
+//     (async function () {
+//         try {
+//           for(let domain of secrets.domain){
+//               let cloudflare = new CloudFlare(
+//                   domain.API_TOKEN,
+//                   domain.ZONE_ID,
+//                   domoain.BASE_URL
+//               );
+//               cloudflare.compareIP()
+//             // cloudflare.getIP()
+//           }
+//         } catch (error) {
+//             console.log("IIFE Error:", error);
+//         }
+//     })();
+// }
---
+++ conf/base.js
@@ -0,0 +1,21 @@
+module.exports = {
+    domains: {
+            wireguard: {
+                domain: 'wireguard.teeseng.uk',
+                cloudflare_Dns_Id: "ea93b6859cff64c4574a54912897a1f1",
+                zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
+                baseUrl: "https://api.cloudflare.com/client/v4/zones/",
+            },
+            
+            proxy: {
+                domain: 'proxy.teeseng.uk',
+                cloudflare_Dns_Id: "149403c78d119aecdfcde0bc97998b8e",
+                zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
+                baseUrl: "https://api.cloudflare.com/client/v4/zones/",
+            },
+
+        },
+}
+
+
+
